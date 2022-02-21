676e4ad7b4311ed3e8292a63bd48a7a9a1a6c9a4
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-02-22 03:47:45 +0530

    Refactor to es6 syntax

--- myApp.js
+++ myApp.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/views/index.html');
-});
+app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
 
 app.use('/public', express.static(__dirname + '/public'));
 
