2838be6c295f0320813dc7dc5b1d43ef2dcea349
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-02-22 03:50:15 +0530

    Serve json data

--- myApp.js
+++ myApp.js
@@ -5,4 +5,6 @@ app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
 
 app.use('/public', express.static(__dirname + '/public'));
 
+app.get('/json', (req, res) => res.json({message: 'Hello json'}));
+
 module.exports = app;
