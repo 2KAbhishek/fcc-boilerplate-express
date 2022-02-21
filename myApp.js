ffa16c14a5d12e605f5262531336c4666d44f6bd
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-02-22 03:25:26 +0530

    Serve file on root req.

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello Express');
+    res.sendFile(__dirname + '/views/index.html');
 });
 
 module.exports = app;
