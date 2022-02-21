aa97e6a38b0057fcff920cb1b555b148d88c9324
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-02-22 04:01:37 +0530

    Update json response

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 var express = require('express');
 var app = express();
 
@@ -5,6 +6,12 @@ app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
 
 app.use('/public', express.static(__dirname + '/public'));
 
-app.get('/json', (req, res) => res.json({message: 'Hello json'}));
+app.get('/json', (req, res) => {
+    if (process.env.MESSAGE_STYLE === 'uppercase') {
+        res.json({message: 'Hello json'.toUpperCase()});
+    } else {
+        res.json({message: 'Hello json'});
+    }
+});
 
 module.exports = app;
