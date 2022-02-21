3fd0e0cabc54f1bae2b2e9d5fd21028814364992
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-02-22 03:14:16 +0530

    Serve hello express response

--- myApp.js
+++ myApp.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
-console.log('Hello World');
+app.get('/', function (req, res) {
+    res.send('Hello Express');
+});
 
 module.exports = app;
