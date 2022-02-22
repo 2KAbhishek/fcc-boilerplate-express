b63fad5e2c62b2a31492a1f395aa7dd1c427926e
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-02-22 23:28:43 +0530

    Add root level reqeust logger middleware.

--- myApp.js
+++ myApp.js
@@ -2,15 +2,20 @@ require('dotenv').config();
 var express = require('express');
 var app = express();
 
+app.use((req, res, next) => {
+    console.log(req.method, req.path, " - ", req.ip);
+    next();
+})
+
 app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
 
 app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/json', (req, res) => {
     if (process.env.MESSAGE_STYLE === 'uppercase') {
-        res.json({message: 'Hello json'.toUpperCase()});
+        res.json({ message: 'Hello json'.toUpperCase() });
     } else {
-        res.json({message: 'Hello json'});
+        res.json({ message: 'Hello json' });
     }
 });
 
