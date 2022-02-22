0b3378e10be67225e822addf037add213f1ad80d
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-02-22 23:55:23 +0530

    Use res.send with json data.

--- myApp.js
+++ myApp.js
@@ -23,7 +23,7 @@ app.get('/now', (req, res, next) => {
     req.time = new Date().toString();
     next();
 }, (req, res) => {
-    res.json({ time: req.time })
+    res.send({ time: req.time })
 })
 
 module.exports = app;
