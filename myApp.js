6086951ae13496717270a6cb6837de4efeffdd0f
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-02-22 12:25:21 +0530

    Update case conditional

--- myApp.js
+++ myApp.js
@@ -9,7 +9,7 @@ app.use('/public', express.static(__dirname + '/public'));
 app.get('/json', (req, res) => {
     if (process.env.MESSAGE_STYLE === 'uppercase') {
         res.json({message: 'Hello json'.toUpperCase()});
-    } else if (process.env.MESSAGE_STYLE === 'lowercase') {
+    } else {
         res.json({message: 'Hello json'});
     }
 });
