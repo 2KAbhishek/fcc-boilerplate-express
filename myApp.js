5aa10b153031eb467114801ad63d0afc52357f69
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-02-23 01:24:00 +0530

    Add post request handler for /name

--- myApp.js
+++ myApp.js
@@ -31,6 +31,8 @@ app.get('/now', curTime, (req, res) => res.send({ time: req.time }));
 
 app.get('/:word/echo', (req, res) => res.json({ echo: req.params.word }));
 
-app.get('/name', (req, res) => res.json({ name: `${req.query.first} ${req.query.last}` }));
+app.route('/name')
+    .get((req, res) => res.json({ name: `${req.query.first} ${req.query.last}` }))
+    .post((req, res) => res.json({ name: `${req.body.first} ${req.body.last}` }));
 
 module.exports = app;
