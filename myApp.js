28ad3c9ca1afff80e0df0ac01f90d15eebb148b7
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-02-22 23:51:42 +0530

    Chain middleware for /now route.

--- myApp.js
+++ myApp.js
@@ -19,4 +19,11 @@ app.get('/json', (req, res) => {
     }
 });
 
+app.get('/now', (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+}, (req, res) => {
+    res.json({ time: req.time })
+})
+
 module.exports = app;
